0a9043ead846db0b705ee31f9d761b835bb71566
kurahashi <koki.kurahashi@di-v.co.jp>
2021-02-23 08:49:03 +0900

    関数の汎用化

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,21 +1,27 @@
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import ErrorIcon from '@material-ui/icons/Error';
 import './index.css';
-import React, { useState } from "react"
-
+import React, { useState } from 'react'
 export default function Header() {
   const [isOpenedMenu, setIsOpenedMenu] = useState(true)
+  const [isOpenedError, setIsOpenedError] = useState(true)
+  // ここが複数個出るのは許して
 
-  const handleIsOpenedMenu = () => {
-    setIsOpenedMenu(!isOpenedMenu)
+  const handleIsOpened = (IsOpenedState, setIsOpenedState) => {
+    setIsOpenedState(!IsOpenedState)
   }
 
   return (
     <div>
       <div className='header'>ヘッダー
         <AddCircleOutlineIcon
-          onClick = { () => handleIsOpenedMenu() }
+          onClick = { () => handleIsOpened(isOpenedMenu, setIsOpenedMenu) }
         />
         { isOpenedMenu && <div className="menu"> メニュー </div> }
+        <ErrorIcon
+          onClick = { () => handleIsOpened(isOpenedError, setIsOpenedError ) }
+        />
+        { isOpenedError && <div className="menu"> メニュー </div> }
       </div>
     </div>
   );
